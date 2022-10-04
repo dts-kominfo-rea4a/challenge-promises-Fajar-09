d38bc7f934e6fe49a0f3478b075f9775806fab62
Fajar-09 <fajarrh2002@gmail.com>
2022-10-04 11:48:21 +0700

    Merubah promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,36 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise((resolve) => {
+    let marah = 0;
+    let tidakMarah = 0;
+    (async () => {
+      const IXX = await promiseTheaterIXX();
+      const VGC = await promiseTheaterVGC();
+
+      for (let i = 0; i < IXX.length; i++) {
+        if (IXX[i].hasil === "marah") {
+          marah++;
+        } else {
+          tidakMarah++;
+        }
+      }
+      for (let i = 0; i < VGC.length; i++) {
+        if (VGC[i].hasil === "marah") {
+          marah++;
+        } else {
+          tidakMarah++;
+        }
+      }
+      if (emosi === "marah") {
+        resolve(marah);
+      } else {
+        resolve(tidakMarah);
+      }
+    })();
+  });
+};
 
 module.exports = {
   promiseOutput,
