7e215fe6614e8b12e994475124288f24583993cf
Fajar-09 <fajarrh2002@gmail.com>
2022-10-05 07:45:31 +0700

    merubah cara looping di promise.js

--- promise.js
+++ promise.js
@@ -5,33 +5,66 @@ const promiseOutput = (emosi) => {
   return new Promise((resolve) => {
     let marah = 0;
     let tidakMarah = 0;
-    (async () => {
-      const IXX = await promiseTheaterIXX();
-      const VGC = await promiseTheaterVGC();
 
-      for (let i = 0; i < IXX.length; i++) {
-        if (IXX[i].hasil === "marah") {
-          marah++;
-        } else {
-          tidakMarah++;
+    promiseTheaterIXX()
+      .then((data) => data)
+      .then((end) => {
+        for (let i = 0; i < end.length; i++) {
+          if (end[i].hasil === "marah") {
+            marah++;
+          } else {
+            tidakMarah++;
+          }
         }
-      }
-      for (let i = 0; i < VGC.length; i++) {
-        if (VGC[i].hasil === "marah") {
-          marah++;
-        } else {
-          tidakMarah++;
-        }
-      }
-      if (emosi === "marah") {
-        resolve(marah);
-      } else {
-        resolve(tidakMarah);
-      }
-    })();
+
+        promiseTheaterVGC()
+          .then((data) => data)
+          .then((end) => {
+            for (let i = 0; i < end.length; i++) {
+              if (end[i].hasil === "marah") {
+                marah++;
+              } else {
+                tidakMarah++;
+              }
+            }
+
+            if (emosi === "marah") {
+              resolve(marah);
+            } else {
+              resolve(tidakMarah);
+            }
+          });
+      });
   });
 };
 
+//CARA KE-2 MENGGUNAKAN ASYNC AWAIT
+// let marah = 0;
+// let tidakMarah = 0;
+// (async () => {
+//   const IXX = await promiseTheaterIXX();
+//   const VGC = await promiseTheaterVGC();
+
+//   for (let i = 0; i < IXX.length; i++) {
+//     if (IXX[i].hasil === "marah") {
+//       marah++;
+//     } else {
+//       tidakMarah++;
+//     }
+//   }
+//   for (let i = 0; i < VGC.length; i++) {
+//     if (VGC[i].hasil === "marah") {
+//       marah++;
+//     } else {
+//       tidakMarah++;
+//     }
+//   }
+//   if (emosi === "marah") {
+//     resolve(marah);
+//   } else {
+//     resolve(tidakMarah);
+//   }
+// })();
 module.exports = {
   promiseOutput,
 };
